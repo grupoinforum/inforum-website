8ca9e739a2d37e24e88abdd68d1ecbb2a9635d0b
grupoinforum <tu-correo@lo-que-uses.com>
2025-09-26 14:52:48 -0600

    chore: mover viewport a _app y limpieza en _document

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,23 @@
-import './style.css'
+// pages/_app.js
+import Head from 'next/head';
+import './style.css';
 
-import { GlobalProvider } from '../global-context'
-import { NextIntlProvider } from 'next-intl'
+import { GlobalProvider } from '../global-context';
+import { NextIntlProvider } from 'next-intl';
+
 export default function MyApp({ Component, pageProps }) {
   return (
-    <NextIntlProvider messages={pageProps?.messages}>
-      <GlobalProvider>
-        <Component {...pageProps} />
-      </GlobalProvider>
-    </NextIntlProvider>
-  )
+    <>
+      {/* Aquí va el viewport (¡ya no en _document.js!) */}
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+      <NextIntlProvider messages={pageProps?.messages}>
+        <GlobalProvider>
+          <Component {...pageProps} />
+        </GlobalProvider>
+      </NextIntlProvider>
+    </>
+  );
 }
